a38e94e70f5e1b0fd7a6e81f71f7bcc4c0a74a96
Carlos Theran <carlos.theran@upr.edu>
2019-07-24 21:38:59 -0400

    file deleted

--- download_hamptdata_python2.py
+++
@@ -1,8 +0,0 @@
-import urllib2
-
-filedata = urllib2.urlopen('https://www.hydroshare.org/resource/9e1b23607ac240588ba50d6b5b9a49b5/data/contents/hampt_rd_data.sqlite')
-datatowrite = filedata.read()
- 
-with open('/home/carlos88/output_hydrology/data.sqlite', 'wb') as f:
-    f.write(datatowrite)
-
